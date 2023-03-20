02f149082ef688df355d5efdcd9885b373e0ed99
nikhilepparla <e.nikhil1999@gmail.com>
2023-03-20 22:13:37 +0530

    pre and post increment

--- First.c
+++ First.c
@@ -1,16 +1,30 @@
 #include <stdio.h>
+int a, b, c = 0;
+void preIncrement();
+void postIncrement();
+
 int main()
 {
-    float f, c;
-    int low = 0, high = 300, inc = 20;
-
-    c = low;
-    while (c <=  high)
-    {
-        f = ((c * 9.0) / 5) + 32;
-        printf("%.2f\t%.2f\n", c, f);
-        c += inc;
-    }
-
+    preIncrement();
+    postIncrement();
     return 0;
+}
+
+void preIncrement(void)
+{
+    //first a =1
+    int a = 1;
+    //++a pre increment a = 2+2
+    //++a is having higher presedence so first the value is incremented and then assigned to a
+    a += ++a;
+    printf("%d\n", a);
+}
+void postIncrement(void)
+{
+    //first a=1;
+    //a++ post incremented a=2+1;
+    //here a++ post incremented first the value is assigned then it gets incremented 
+    int a = 1;
+    a += a++;
+    printf("%d", a);
 }
